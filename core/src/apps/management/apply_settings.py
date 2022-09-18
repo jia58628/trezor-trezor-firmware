a2e4a4e4e0f75e14dc05f6772f7696b2d82b10be
grdddj <jiri.musil06@seznam.cz>
2022-09-18 16:20:10 +0200

    chore(core): decrease management size by 1440 bytes

--- core/src/apps/management/apply_settings.py
+++ core/src/apps/management/apply_settings.py
@@ -1,243 +1,258 @@
 from typing import TYPE_CHECKING
 
-import storage.device
-from trezor import ui, wire
-from trezor.enums import ButtonRequestType, SafetyCheckLevel
-from trezor.messages import Success
-from trezor.strings import format_duration_ms
+from trezor.enums import ButtonRequestType
 from trezor.ui.layouts import confirm_action
 
-from apps.base import reload_settings_from_storage
-from apps.common import safety_checks
-
 if TYPE_CHECKING:
-    from trezor.messages import ApplySettings
+    from trezor.messages import ApplySettings, Success
+    from trezor.wire import Context, GenericContext
+    from trezor.enums import SafetyCheckLevel
 
 
-def validate_homescreen(homescreen: bytes) -> None:
+BRT_PROTECT_CALL = ButtonRequestType.ProtectCall  # CACHE
+
+
+def _validate_homescreen(homescreen: bytes) -> None:
+    from trezor import ui
+    import storage.device as storage_device
+    from trezor.wire import DataError
+
     if homescreen == b"":
         return
 
-    if len(homescreen) > storage.device.HOMESCREEN_MAXSIZE:
-        raise wire.DataError(
-            f"Homescreen is too large, maximum size is {storage.device.HOMESCREEN_MAXSIZE} bytes"
+    if len(homescreen) > storage_device.HOMESCREEN_MAXSIZE:
+        raise DataError(
+            f"Homescreen is too large, maximum size is {storage_device.HOMESCREEN_MAXSIZE} bytes"
         )
 
     try:
         w, h, grayscale = ui.display.toif_info(homescreen)
     except ValueError:
-        raise wire.DataError("Invalid homescreen")
+        raise DataError("Invalid homescreen")
     if w != 144 or h != 144:
-        raise wire.DataError("Homescreen must be 144x144 pixel large")
+        raise DataError("Homescreen must be 144x144 pixel large")
     if grayscale:
-        raise wire.DataError("Homescreen must be full-color TOIF image")
+        raise DataError("Homescreen must be full-color TOIF image")
 
 
-async def apply_settings(ctx: wire.Context, msg: ApplySettings) -> Success:
-    if not storage.device.is_initialized():
-        raise wire.NotInitialized("Device is not initialized")
+async def apply_settings(ctx: Context, msg: ApplySettings) -> Success:
+    import storage.device as storage_device
+    from apps.common import safety_checks
+    from trezor.messages import Success
+    from trezor.wire import DataError, ProcessError, NotInitialized
+    from apps.base import reload_settings_from_storage
+
+    if not storage_device.is_initialized():
+        raise NotInitialized("Device is not initialized")
+
+    msg_homescreen = msg.homescreen  # cache
+    msg_label = msg.label  # cache
+    msg_auto_lock_delay_ms = msg.auto_lock_delay_ms  # cache
+    msg_use_passphrase = msg.use_passphrase  # cache
+    msg_passphrase_always_on_device = msg.passphrase_always_on_device  # cache
+    msg_display_rotation = msg.display_rotation  # cache
+    msg_safety_checks = msg.safety_checks  # cache
+    msg_experimental_features = msg.experimental_features  # cache
+
     if (
-        msg.homescreen is None
-        and msg.label is None
-        and msg.use_passphrase is None
-        and msg.passphrase_always_on_device is None
-        and msg.display_rotation is None
-        and msg.auto_lock_delay_ms is None
-        and msg.safety_checks is None
-        and msg.experimental_features is None
+        msg_homescreen is None
+        and msg_label is None
+        and msg_use_passphrase is None
+        and msg_passphrase_always_on_device is None
+        and msg_display_rotation is None
+        and msg_auto_lock_delay_ms is None
+        and msg_safety_checks is None
+        and msg_experimental_features is None
     ):
-        raise wire.ProcessError("No setting provided")
+        raise ProcessError("No setting provided")
 
-    if msg.homescreen is not None:
-        validate_homescreen(msg.homescreen)
-        await require_confirm_change_homescreen(ctx)
+    if msg_homescreen is not None:
+        _validate_homescreen(msg_homescreen)
+        await _require_confirm_change_homescreen(ctx)
         try:
-            storage.device.set_homescreen(msg.homescreen)
+            storage_device.set_homescreen(msg_homescreen)
         except ValueError:
-            raise wire.DataError("Invalid homescreen")
+            raise DataError("Invalid homescreen")
 
-    if msg.label is not None:
-        if len(msg.label) > storage.device.LABEL_MAXLENGTH:
-            raise wire.DataError("Label too long")
-        await require_confirm_change_label(ctx, msg.label)
-        storage.device.set_label(msg.label)
+    if msg_label is not None:
+        if len(msg_label) > storage_device.LABEL_MAXLENGTH:
+            raise DataError("Label too long")
+        await _require_confirm_change_label(ctx, msg_label)
+        storage_device.set_label(msg_label)
 
-    if msg.use_passphrase is not None:
-        await require_confirm_change_passphrase(ctx, msg.use_passphrase)
-        storage.device.set_passphrase_enabled(msg.use_passphrase)
+    if msg_use_passphrase is not None:
+        await _require_confirm_change_passphrase(ctx, msg_use_passphrase)
+        storage_device.set_passphrase_enabled(msg_use_passphrase)
 
-    if msg.passphrase_always_on_device is not None:
-        if not storage.device.is_passphrase_enabled():
-            raise wire.DataError("Passphrase is not enabled")
-        await require_confirm_change_passphrase_source(
-            ctx, msg.passphrase_always_on_device
+    if msg_passphrase_always_on_device is not None:
+        if not storage_device.is_passphrase_enabled():
+            raise DataError("Passphrase is not enabled")
+        await _require_confirm_change_passphrase_source(
+            ctx, msg_passphrase_always_on_device
         )
-        storage.device.set_passphrase_always_on_device(msg.passphrase_always_on_device)
+        storage_device.set_passphrase_always_on_device(msg_passphrase_always_on_device)
 
-    if msg.auto_lock_delay_ms is not None:
-        if msg.auto_lock_delay_ms < storage.device.AUTOLOCK_DELAY_MINIMUM:
-            raise wire.ProcessError("Auto-lock delay too short")
-        if msg.auto_lock_delay_ms > storage.device.AUTOLOCK_DELAY_MAXIMUM:
-            raise wire.ProcessError("Auto-lock delay too long")
-        await require_confirm_change_autolock_delay(ctx, msg.auto_lock_delay_ms)
-        storage.device.set_autolock_delay_ms(msg.auto_lock_delay_ms)
+    if msg_auto_lock_delay_ms is not None:
+        if msg_auto_lock_delay_ms < storage_device.AUTOLOCK_DELAY_MINIMUM:
+            raise ProcessError("Auto-lock delay too short")
+        if msg_auto_lock_delay_ms > storage_device.AUTOLOCK_DELAY_MAXIMUM:
+            raise ProcessError("Auto-lock delay too long")
+        await _require_confirm_change_autolock_delay(ctx, msg_auto_lock_delay_ms)
+        storage_device.set_autolock_delay_ms(msg_auto_lock_delay_ms)
 
-    if msg.safety_checks is not None:
-        await require_confirm_safety_checks(ctx, msg.safety_checks)
-        safety_checks.apply_setting(msg.safety_checks)
+    if msg_safety_checks is not None:
+        await _require_confirm_safety_checks(ctx, msg_safety_checks)
+        safety_checks.apply_setting(msg_safety_checks)
 
-    if msg.display_rotation is not None:
-        await require_confirm_change_display_rotation(ctx, msg.display_rotation)
-        storage.device.set_rotation(msg.display_rotation)
+    if msg_display_rotation is not None:
+        await _require_confirm_change_display_rotation(ctx, msg_display_rotation)
+        storage_device.set_rotation(msg_display_rotation)
 
-    if msg.experimental_features is not None:
-        await require_confirm_experimental_features(ctx, msg.experimental_features)
-        storage.device.set_experimental_features(msg.experimental_features)
+    if msg_experimental_features is not None:
+        await _require_confirm_experimental_features(ctx, msg_experimental_features)
+        storage_device.set_experimental_features(msg_experimental_features)
 
     reload_settings_from_storage()
 
     return Success(message="Settings applied")
 
 
-async def require_confirm_change_homescreen(ctx: wire.GenericContext) -> None:
+async def _require_confirm_change_homescreen(ctx: GenericContext) -> None:
     await confirm_action(
         ctx,
         "set_homescreen",
         "Set homescreen",
         description="Do you really want to change the homescreen image?",
-        br_code=ButtonRequestType.ProtectCall,
+        br_code=BRT_PROTECT_CALL,
     )
 
 
-async def require_confirm_change_label(ctx: wire.GenericContext, label: str) -> None:
+async def _require_confirm_change_label(ctx: GenericContext, label: str) -> None:
     await confirm_action(
         ctx,
         "set_label",
         "Change label",
         description="Do you really want to change the label to {}?",
         description_param=label,
-        br_code=ButtonRequestType.ProtectCall,
+        br_code=BRT_PROTECT_CALL,
     )
 
 
-async def require_confirm_change_passphrase(
-    ctx: wire.GenericContext, use: bool
-) -> None:
-    if use:
-        description = "Do you really want to enable passphrase encryption?"
-    else:
-        description = "Do you really want to disable passphrase encryption?"
+async def _require_confirm_change_passphrase(ctx: GenericContext, use: bool) -> None:
+    template = "Do you really want to {} passphrase encryption?"
+    description = template.format("enable" if use else "disable")
     await confirm_action(
         ctx,
         "set_passphrase",
         "Enable passphrase" if use else "Disable passphrase",
         description=description,
-        br_code=ButtonRequestType.ProtectCall,
+        br_code=BRT_PROTECT_CALL,
     )
 
 
-async def require_confirm_change_passphrase_source(
-    ctx: wire.GenericContext, passphrase_always_on_device: bool
+async def _require_confirm_change_passphrase_source(
+    ctx: GenericContext, passphrase_always_on_device: bool
 ) -> None:
-    if passphrase_always_on_device:
-        description = "Do you really want to enter passphrase always on the device?"
-    else:
-        description = "Do you want to revoke the passphrase on device setting?"
+    description = (
+        "Do you really want to enter passphrase always on the device?"
+        if passphrase_always_on_device
+        else "Do you want to revoke the passphrase on device setting?"
+    )
     await confirm_action(
         ctx,
         "set_passphrase_source",
         "Passphrase source",
         description=description,
-        br_code=ButtonRequestType.ProtectCall,
+        br_code=BRT_PROTECT_CALL,
     )
 
 
-async def require_confirm_change_display_rotation(
-    ctx: wire.GenericContext, rotation: int
+async def _require_confirm_change_display_rotation(
+    ctx: GenericContext, rotation: int
 ) -> None:
-    if rotation == 0:
-        label = "north"
-    elif rotation == 90:
-        label = "east"
-    elif rotation == 180:
-        label = "south"
-    elif rotation == 270:
-        label = "west"
-    else:
-        raise wire.DataError("Unsupported display rotation")
+    from trezor.wire import DataError
+
+    label = {
+        0: "north",
+        90: "east",
+        180: "south",
+        270: "west",
+    }.get(rotation)
+    if label is None:
+        raise DataError("Unsupported display rotation")
     await confirm_action(
         ctx,
         "set_rotation",
         "Change rotation",
         description="Do you really want to change display rotation to {}?",
         description_param=label,
-        br_code=ButtonRequestType.ProtectCall,
+        br_code=BRT_PROTECT_CALL,
     )
 
 
-async def require_confirm_change_autolock_delay(
-    ctx: wire.GenericContext, delay_ms: int
+async def _require_confirm_change_autolock_delay(
+    ctx: GenericContext, delay_ms: int
 ) -> None:
+    from trezor.strings import format_duration_ms
+
     await confirm_action(
         ctx,
         "set_autolock_delay",
         "Auto-lock delay",
         description="Do you really want to auto-lock your device after {}?",
         description_param=format_duration_ms(delay_ms),
-        br_code=ButtonRequestType.ProtectCall,
+        br_code=BRT_PROTECT_CALL,
     )
 
 
-async def require_confirm_safety_checks(
-    ctx: wire.GenericContext, level: SafetyCheckLevel
+async def _require_confirm_safety_checks(
+    ctx: GenericContext, level: SafetyCheckLevel
 ) -> None:
-    if level == SafetyCheckLevel.PromptAlways:
-        await confirm_action(
-            ctx,
-            "set_safety_checks",
-            "Safety override",
-            hold=True,
-            verb="Hold to confirm",
-            description="Trezor will allow you to approve some actions which might be unsafe.",
-            action="Are you sure?",
-            reverse=True,
-            larger_vspace=True,
-            br_code=ButtonRequestType.ProtectCall,
-        )
-    elif level == SafetyCheckLevel.PromptTemporarily:
-        await confirm_action(
-            ctx,
-            "set_safety_checks",
-            "Safety override",
-            hold=True,
-            verb="Hold to confirm",
-            description="Trezor will temporarily allow you to approve some actions which might be unsafe.",
-            action="Are you sure?",
-            reverse=True,
-            br_code=ButtonRequestType.ProtectCall,
-        )
-    elif level == SafetyCheckLevel.Strict:
+    from trezor.enums import SafetyCheckLevel
+
+    if level == SafetyCheckLevel.Strict:
         await confirm_action(
             ctx,
             "set_safety_checks",
             "Safety checks",
             description="Do you really want to enforce strict safety checks (recommended)?",
-            br_code=ButtonRequestType.ProtectCall,
+            br_code=BRT_PROTECT_CALL,
+        )
+    elif level in (SafetyCheckLevel.PromptAlways, SafetyCheckLevel.PromptTemporarily):
+        # Reusing most stuff for both levels
+        template = (
+            "Trezor will{}allow you to approve some actions which might be unsafe."
+        )
+        description = template.format(
+            " temporarily " if level == SafetyCheckLevel.PromptTemporarily else " "
+        )
+
+        await confirm_action(
+            ctx,
+            "set_safety_checks",
+            "Safety override",
+            "Are you sure?",
+            description,
+            hold=True,
+            verb="Hold to confirm",
+            reverse=True,
+            larger_vspace=level == SafetyCheckLevel.PromptAlways,
+            br_code=BRT_PROTECT_CALL,
         )
     else:
         raise ValueError  # enum value out of range
 
 
-async def require_confirm_experimental_features(
-    ctx: wire.GenericContext, enable: bool
+async def _require_confirm_experimental_features(
+    ctx: GenericContext, enable: bool
 ) -> None:
     if enable:
         await confirm_action(
             ctx,
             "set_experimental_features",
             "Experimental mode",
-            description="Enable experimental features?",
-            action="Only for development and beta testing!",
+            "Only for development and beta testing!",
+            "Enable experimental features?",
             reverse=True,
-            br_code=ButtonRequestType.ProtectCall,
+            br_code=BRT_PROTECT_CALL,
         )
--- core/src/apps/management/recovery_device/recover.py
+++ core/src/apps/management/recovery_device/recover.py
@@ -1,11 +1,8 @@
 from typing import TYPE_CHECKING
 
-import storage.recovery
-import storage.recovery_shares
-from trezor.crypto import bip39, slip39
-from trezor.errors import MnemonicError
-
-from .. import backup_types
+import storage.recovery as storage_recovery
+import storage.recovery_shares as storage_recovery_shares
+from trezor.crypto import slip39
 
 if TYPE_CHECKING:
     from trezor.enums import BackupType
@@ -20,6 +17,9 @@ def process_bip39(words: str) -> bytes:
     Receives single mnemonic and processes it. Returns what is then stored
     in the storage, which is the mnemonic itself for BIP-39.
     """
+    from trezor.crypto import bip39
+    from trezor.errors import MnemonicError
+
     if not bip39.check(words):
         raise MnemonicError
     return words.encode()
@@ -32,17 +32,19 @@ def process_slip39(words: str) -> tuple[bytes | None, slip39.Share]:
     """
     share = slip39.decode_mnemonic(words)
 
-    remaining = storage.recovery.fetch_slip39_remaining_shares()
+    recovery_shares = storage_recovery_shares  # cache
+    recovery = storage_recovery  # cache
+    share_group_index = share.group_index  # cache
+
+    remaining = recovery.fetch_slip39_remaining_shares()
 
     # if this is the first share, parse and store metadata
     if not remaining:
-        storage.recovery.set_slip39_group_count(share.group_count)
-        storage.recovery.set_slip39_iteration_exponent(share.iteration_exponent)
-        storage.recovery.set_slip39_identifier(share.identifier)
-        storage.recovery.set_slip39_remaining_shares(
-            share.threshold - 1, share.group_index
-        )
-        storage.recovery_shares.set(share.index, share.group_index, words)
+        recovery.set_slip39_group_count(share.group_count)
+        recovery.set_slip39_iteration_exponent(share.iteration_exponent)
+        recovery.set_slip39_identifier(share.identifier)
+        recovery.set_slip39_remaining_shares(share.threshold - 1, share_group_index)
+        recovery_shares.set(share.index, share_group_index, words)
 
         # if share threshold and group threshold are 1
         # we can calculate the secret right away
@@ -54,24 +56,21 @@ def process_slip39(words: str) -> tuple[bytes | None, slip39.Share]:
             return None, share
 
     # These should be checked by UI before so it's a Runtime exception otherwise
-    if share.identifier != storage.recovery.get_slip39_identifier():
+    if share.identifier != recovery.get_slip39_identifier():
         raise RuntimeError("Slip39: Share identifiers do not match")
-    if share.iteration_exponent != storage.recovery.get_slip39_iteration_exponent():
+    if share.iteration_exponent != recovery.get_slip39_iteration_exponent():
         raise RuntimeError("Slip39: Share exponents do not match")
-    if storage.recovery_shares.get(share.index, share.group_index):
+    if recovery_shares.get(share.index, share_group_index):
         raise RuntimeError("Slip39: This mnemonic was already entered")
-    if share.group_count != storage.recovery.get_slip39_group_count():
+    if share.group_count != recovery.get_slip39_group_count():
         raise RuntimeError("Slip39: Group count does not match")
 
     remaining_for_share = (
-        storage.recovery.get_slip39_remaining_shares(share.group_index)
-        or share.threshold
+        recovery.get_slip39_remaining_shares(share_group_index) or share.threshold
     )
-    storage.recovery.set_slip39_remaining_shares(
-        remaining_for_share - 1, share.group_index
-    )
-    remaining[share.group_index] = remaining_for_share - 1
-    storage.recovery_shares.set(share.index, share.group_index, words)
+    recovery.set_slip39_remaining_shares(remaining_for_share - 1, share_group_index)
+    remaining[share_group_index] = remaining_for_share - 1
+    recovery_shares.set(share.index, share_group_index, words)
 
     if remaining.count(0) < share.group_threshold:
         # we need more shares
@@ -82,11 +81,11 @@ def process_slip39(words: str) -> tuple[bytes | None, slip39.Share]:
         for i, r in enumerate(remaining):
             # if we have multiple groups pass only the ones with threshold reached
             if r == 0:
-                group = storage.recovery_shares.fetch_group(i)
+                group = recovery_shares.fetch_group(i)
                 mnemonics.extend(group)
     else:
         # in case of slip39 basic we only need the first and only group
-        mnemonics = storage.recovery_shares.fetch_group(0)
+        mnemonics = recovery_shares.fetch_group(0)
 
     _, _, secret = slip39.recover_ems(mnemonics)
     return secret, share
@@ -97,6 +96,8 @@ if TYPE_CHECKING:
 
 
 def load_slip39_state() -> Slip39State:
+    from .. import backup_types
+
     previous_mnemonics = fetch_previous_mnemonics()
     if not previous_mnemonics:
         return None, None
@@ -109,10 +110,10 @@ def load_slip39_state() -> Slip39State:
 
 def fetch_previous_mnemonics() -> list[list[str]] | None:
     mnemonics = []
-    if not storage.recovery.get_slip39_group_count():
+    if not storage_recovery.get_slip39_group_count():
         return None
-    for i in range(storage.recovery.get_slip39_group_count()):
-        mnemonics.append(storage.recovery_shares.fetch_group(i))
+    for i in range(storage_recovery.get_slip39_group_count()):
+        mnemonics.append(storage_recovery_shares.fetch_group(i))
     if not any(p for p in mnemonics):
         return None
     return mnemonics
